491e176b436b1ba203eb5ab04d69940342123d90
Chris Wamae <chriswamae123@gmail.com>
2023-04-14 14:54:51 +0300

    Update tests_controller.rb

--- backend/app/controllers/tests_controller.rb
+++ backend/app/controllers/tests_controller.rb
@@ -1,5 +1,5 @@
 class TestsController < ApplicationController
     def index
-        render json: {workflow:"CI: setting up schema test."}
+        render json: {workflow:"CI: setting up schema test"}
     end
 end
