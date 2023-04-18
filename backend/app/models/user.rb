fb86f51c615f9b3643d315e5c423593f37c566e8
Bernard <bernard.koech@student.moringaschool.com>
2023-04-18 15:33:50 +0300

    Added relations for User and Reminder model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :donations (foreign key: donor_id)
+  has_many :charity_donations, class_name: "Donation", foreign_key: "charity_id"
+  has_many :beneficiaries, foreign_key: "charity_id"
+  has_many :inventories, foreign_key: "charity_id"
+  has_many :donor_reminders, class_name: "Reminder", foreign_key: "donor_id"
+  has_many :charity_reminders, class_name: "Reminder", foreign_key: "charity_id"
 end
