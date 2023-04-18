375bfea755fbcf1fee8792ce39cff575bba16cc7
Bernard <bernard.koech@student.moringaschool.com>
2023-04-18 13:26:40 +0300

    added tests for Beneficiary and Donation

--- backend/test/models/beneficiary_test.rb
+++ backend/test/models/beneficiary_test.rb
@@ -1,7 +1,14 @@
-require "test_helper"
+require 'test_helper'
 
 class BeneficiaryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid" do
+    assert build(:beneficiary).valid?
+  end
+
+  test "should validate presence of name, age, story, charity_id, and donor_id" do
+    beneficiary = Beneficiary.new
+    assert_not beneficiary.valid?
+    assert_equal [:name, :age, :story, :charity_id, :donor_id], beneficiary.errors.keys
+  end
 end
+
--- backend/test/models/donation_test.rb
+++ backend/test/models/donation_test.rb
@@ -1,7 +1,14 @@
-require "test_helper"
+require 'test_helper'
 
 class DonationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid" do
+    assert build(:donation).valid?
+  end
+
+  test "should validate presence of donor_id, charity_id, and amount" do
+    donation = Donation.new
+    assert_not donation.valid?
+    assert_equal [:donor_id, :charity_id, :amount], donation.errors.keys
+  end
+
 end
