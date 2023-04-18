7d4565904ede5dd70704a848c48b98eb9ee2d7de
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-04-19 00:07:06 +0300

    added donor model associations

--- backend/app/models/donor.rb
+++ backend/app/models/donor.rb
@@ -1,2 +1,6 @@
 class Donor < User
+    has_many :donations, foreign_key: :donor_id
+    has_many :charities, through: :donations
+    has_many :beneficiaries, through: :donations
+    has_many :reminders
 end
