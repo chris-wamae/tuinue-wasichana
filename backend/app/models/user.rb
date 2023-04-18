94ca957ff0a3c5f46d588b564f110c53193ebed8
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-04-19 00:27:25 +0300

    defined enums in user model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
-  # has_many :donations (foreign key: donor_id)
-  # has_many :charity_donations, class_name: "Donation", foreign_key: "charity_id"
-  # has_many :beneficiaries, foreign_key: "charity_id"
-  # has_many :inventories, foreign_key: "charity_id"
-  # has_many :donor_reminders, class_name: "Reminder", foreign_key: "donor_id"
-  # has_many :charity_reminders, class_name: "Reminder", foreign_key: "charity_id"
+  enum role: { donor: 0, charity: 1, admin: 2 }
+  
+  #status for charity only
+  enum status: { pending: 0, approved: 1, rejected: 2 }, _prefix: :charity
 end
