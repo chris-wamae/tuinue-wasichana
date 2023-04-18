ec864e782681403b4814281b6e785dcf70158e58
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-04-19 00:02:16 +0300

    added charity model associations

--- backend/app/models/charity.rb
+++ backend/app/models/charity.rb
@@ -1,2 +1,7 @@
 class Charity < User
+    has_many :donations, foreign_key: 'charity_id'
+    has_many :donors, through: :donations
+    has_many :beneficiaries, foreign_key: 'charity_id'
+    has_many :inventories, foreign_key: 'charity_id'
+    has_many :reminders, through: :donors
 end
