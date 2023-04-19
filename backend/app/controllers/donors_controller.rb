1b466fb5e415e329670eb0fed477808fe1ce4846
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-04-19 12:22:24 +0300

    added test for user model

--- backend/app/controllers/donors_controller.rb
+++ backend/app/controllers/donors_controller.rb
@@ -1,5 +1,5 @@
-class DonorsController < ApplicationController
-    def index
-        render json: Charity.all, status: :ok
-    end
-end
+# class DonorsController < ApplicationController
+#     def index
+#         render json: Charity.all, status: :ok
+#     end
+# end
