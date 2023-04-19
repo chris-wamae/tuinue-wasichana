87a2bf61145b940a7858b21137015c2af60c605e
Bernard <bernard.koech@student.moringaschool.com>
2023-04-19 14:31:19 +0300

    Added routes and  controllers for benefiary and donation models

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "tests#index"
+  resources :beneficiaries
+  resources :donations
 end
---
+++ backend/test/controllers/donation_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class DonationControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
