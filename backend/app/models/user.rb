65968cdd88ea8632dc3e78c908a57c9246536e5a
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-04-19 09:17:47 +0300

    defined validations

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   enum role: { donor: 0, charity: 1, admin: 2 }
-  
+
   #status for charity only
   enum status: { pending: 0, approved: 1, rejected: 2 }, _prefix: :charity
+
+  validates :role, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+
 end
