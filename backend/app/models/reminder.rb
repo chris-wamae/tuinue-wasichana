f9dac9f5e064c85cbff22d937678c8a558d5d5e7
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-04-19 09:28:33 +0300

    added validations to reminder and inventory models

--- backend/app/models/reminder.rb
+++ backend/app/models/reminder.rb
@@ -1,4 +1,10 @@
 class Reminder < ApplicationRecord
   belongs_to :donor, class_name: "User"
   belongs_to :charity, class_name: "User"
+
+  validates :date, presence: true
+  validates :time, presence: true
+  validates :donor, presence: true
+  validates :charity, presence: true
+
 end
