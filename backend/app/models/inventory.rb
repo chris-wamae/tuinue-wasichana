7301c9de794e4225592da898f3861a2c6b9ea633
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-04-19 08:27:18 +0300

    added relationships to inventory model

--- backend/app/models/inventory.rb
+++ backend/app/models/inventory.rb
@@ -1,2 +1,5 @@
 class Inventory < ApplicationRecord
+    belongs_to :charity, class_name: 'User'
+    belongs_to :beneficiary
+
 end
--- backend/app/models/reminder.rb
+++ backend/app/models/reminder.rb
@@ -1,4 +1,4 @@
 class Reminder < ApplicationRecord
   belongs_to :donor, class_name: "User"
- belongs_to :charity, class_name: "User"
+  belongs_to :charity, class_name: "User"
 end
