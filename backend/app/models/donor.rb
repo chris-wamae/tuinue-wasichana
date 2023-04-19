b441250faa4dd4af39df18dec4ff37983f0f9f30
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-04-19 12:44:00 +0300

    added test for donor, charity, admin models

--- backend/app/models/donor.rb
+++ backend/app/models/donor.rb
@@ -2,5 +2,5 @@ class Donor < User
     has_many :donations, foreign_key: :donor_id
     has_many :charities, through: :donations
     has_many :beneficiaries, through: :donations
-    has_many :reminders
+    has_many :reminders, foreign_key: :donor_id
 end
