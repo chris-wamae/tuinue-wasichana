682e05aaad47b77b05787b0689b107633b699c65
Bernard <bernard.koech@student.moringaschool.com>
2023-04-20 19:49:31 +0300

    displayed Non-anonymous Donors

--- backend/app/controllers/charity_controller.rb
+++ backend/app/controllers/charity_controller.rb
@@ -6,5 +6,8 @@ class CharityController < ApplicationController
   def show
     @charity = Charity.find(params[:id])
     @total_donations = Donation.where(charity_id: @charity.id).sum(:amount)
+    @non_anonymous_donors = @charity.donations.where.not(donor: nil).map(&:donor).uniq
   end
+
+
 end
