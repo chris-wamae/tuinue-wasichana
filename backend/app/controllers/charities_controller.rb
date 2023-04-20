a029dc0ce174c54a20e4a873cf5c71776f5612a1
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-04-20 18:16:40 +0300

    created display all charities endpoint

--- backend/app/controllers/charities_controller.rb
+++ backend/app/controllers/charities_controller.rb
@@ -1,15 +1,7 @@
 class CharitiesController < ApplicationController
     def index
-        render json: Charity.all, status: :ok
+        render json: User.charity, status: :ok
     end
     
-    def show
-        charity = find_charity
-        render json: charity, status: :ok
-    end
     
-    private
-    def find_charity
-        Charity.find(params[:id])
-    end
 end
