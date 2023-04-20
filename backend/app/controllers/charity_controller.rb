b4780f73ef0eeb3dde18b67207481fb3bbd2ce16
Bernard <bernard.koech@student.moringaschool.com>
2023-04-20 19:40:39 +0300

    displayed the sum of all donations made to each charity

---
+++ backend/app/controllers/charity_controller.rb
@@ -0,0 +1,10 @@
+class CharityController < ApplicationController
+  def index
+    @charities = Charity.all
+  end
+
+  def show
+    @charity = Charity.find(params[:id])
+    @total_donations = Donation.where(charity_id: @charity.id).sum(:amount)
+  end
+end
