66f759d39bd87b1562585becaa360285449c9bef
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-04-21 14:47:50 +0300

    ffinalized sign_in enpoints

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "tests#index"
+  #root "tests#index"
 
   devise_for :users, controllers: {
     registrations: 'registrations'
