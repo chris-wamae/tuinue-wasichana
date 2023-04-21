c119a9f5c827eeec0bd6f94870cdc7d4cc779646
chris-wamae <chriswamae123@gmail.com>
2023-04-21 08:40:04 +0300

    Created routes in App.js for application routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import LandingPage from './views/landing-page/LandingPage';
-import DonationPageMpesa from './views/donations/donation-page-mpesa/DonationPageMpesa';
-import DonationPageCreditCard from './views/donations/donation-page-credit-card/DonationPageCreditCard';
-import DonationPagePayPal from './views/donations/donation-page-paypal/DonationPagePaypal';
-import AdministratorPage from './views/administrator-page/AdministratorPage';
-import CharityApplication from './views/charity-application/CharityApplication';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./views/landing-page/LandingPage";
+import DonationPageMpesa from "./views/donations/donation-page-mpesa/DonationPageMpesa";
+import DonationPageCreditCard from "./views/donations/donation-page-credit-card/DonationPageCreditCard";
+import DonationPagePayPal from "./views/donations/donation-page-paypal/DonationPagePaypal";
+import AdministratorPage from "./views/administrator-page/AdministratorPage";
+import CharityApplication from "./views/charity-application/CharityApplication";
 // import { useDispatch } from 'react-redux';
 // import { fetchCharities } from './features/charity/charitiesSlice.js';
 // import store from './app/store';
@@ -29,14 +30,16 @@ function App() {
   // console.log(char)
 
   return (
-    <>
-      <LandingPage />
-      <DonationPageCreditCard />
-      <DonationPageMpesa />
-      <DonationPagePayPal />
-      <AdministratorPage />
-      <CharityApplication />
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />}></Route>
+        <Route exact path="/credit-card" element={<DonationPageCreditCard />}></Route>
+        <Route exact path="/mpesa" element={<DonationPageMpesa />}></Route>
+        <Route exact path="/paypal" element={<DonationPagePayPal />}></Route>
+        <Route exact path="/admin" element={<AdministratorPage />}></Route>
+        <Route exact path="/charity-application" element={<CharityApplication />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
