f8b85aada095bafbe363e091a65d40ed72aee5de
chris-wamae <chriswamae123@gmail.com>
2023-04-22 16:59:43 +0300

    Created state variables for form inputs

--- frontend/src/views/charity-application/CharityApplication.js
+++ frontend/src/views/charity-application/CharityApplication.js
@@ -1,7 +1,22 @@
 import NavBar from "../../components/navbar/NavBar";
 import "./charity-application.css"
+import {useState } from "react";
 
 function CharityApplication() {
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+}
+
+const [email,setEmail] = useState("")
+const [password,setPassword] = useState("")
+const [passwordConfirm,setPasswordConfirm] = useState("")
+const [charityName,setCharityName] = useState("")
+const [number,setNumber] = useState("")
+const [location,setLocation] = useState("")
+const [image,setImage] = useState("")
+const [description,setDescription] = useState("")
+const [mission,setMission] = useState("")
 
     return(
      <>
@@ -10,30 +25,30 @@ function CharityApplication() {
       Use the form below to apply to be a charity on our platform.
         </h2>
       <div className="donation-page">
-          <form className="pay-form">
+          <form className="pay-form" onSubmit={handleSubmit}>
             <div>
               <h4>Account Creation</h4>
               <div className="contacts">
-                <input type="text" placeholder="Email address"></input>
-                <input type="text" placeholder="Password"></input>
-                <input type="text" placeholder="Password confirmation"></input>
+                <input type="text" placeholder="Email address"  onChange={(e) =>{setEmail(e.target.value)}}></input>
+                <input type="text" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}}></input>
+                <input type="text" placeholder="Password confirmation" onChange={(e) => setPasswordConfirm(e.target.value)}></input>
               </div>
             <p>*You’ll be using these credentials to sign in to the platform as a charity</p>
             </div>
             <div>
               <h4>Your Charity Information</h4>
               <div className="payer-details">
-                <input type="text" placeholder="Charity name"></input>
-                <input type="text" placeholder="Telephone number"></input>
-                <input type="text" placeholder="Charity location"></input>
-                <input type="text" placeholder="Charity image"></input>
+                <input type="text" placeholder="Charity name" onChange={(e) => setCharityName(e.target.value)}></input>
+                <input type="text" placeholder="Telephone number" onChange={(e) =>setNumber(e.target.value)}></input>
+                <input type="text" placeholder="Charity location" onChange={(e) => setLocation(e.target.value)}></input>
+                <input type="text" placeholder="Charity image" onChange={(e) => setImage(e.target.value)}></input>
               </div>
             </div>
             <div className="mission-desc">
-            <input id="charity-mission" type="text-area" placeholder="Charity Mission"></input>
-            <input id="charity-description" type="text-area" placeholder="Charity Description"></input>
+            <input id="charity-mission" type="text-area" placeholder="Charity Mission" onChange={(e)=>setMission(e.target.value)}></input>
+            <input id="charity-description" type="text-area" placeholder="Charity Description" onChange={(e) => setDescription(e.target.value) }></input>
             </div>
-            <button className="donate-button" disabled>Donate</button>
+            <button className="donate-button" type="submit">Apply</button>
           </form>
         <img
           src="/donation-page/pexels-askar-abayev-6189929.jpg"
--- frontend/src/views/donations/donation-page-credit-card/DonationAmountPage.js
+++ frontend/src/views/donations/donation-page-credit-card/DonationAmountPage.js
@@ -4,22 +4,23 @@ import "./donation-page-amount.css";
 import { useNavigate } from "react-router-dom";
 
 function DonationAmountPage() {
-const nagivate = useNavigate()
-
-const handleSubmit = (e) =>{
-e.preventDefault()
-nagivate("/donate")
-
-}
+  const nagivate = useNavigate()
+  const [donationAmount, setDonationAmount] = useState(0)
+  const [charityId,setCharityId] = useState(undefined)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    nagivate("/donate")
+    console.log(donationAmount)
+  }
 
   return (
     <>
-      <NavBar elements={[]} />    
-          <form onSubmit={handleSubmit} className="amount-form">
-              <h4 style={{margin:"10px"}}>Enter donation amount below.</h4>
-                <input required type="text" placeholder="Amount in Ksh."></input>
-            <button className="donate-button" type="submit">Proceed to payment</button>
-          </form>
+      <NavBar elements={[]} />
+      <form onSubmit={handleSubmit} className="amount-form">
+        <h4 style={{ margin: "10px" }}>Enter donation amount below.</h4>
+        <input onChange={(e) => { setDonationAmount(e.target.value) }} required type="number" placeholder="Amount in Ksh."></input>
+        <button className="donate-button" type="submit">Proceed to payment</button>
+      </form>
     </>
   );
 }
