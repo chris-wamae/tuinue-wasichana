f42087a2c158de4ae5a938f60b7d113caaef0662
phylis13 <phylisnjeri54@gmail.com>
2023-04-23 16:17:48 +0300

    changes done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,20 @@
 //import logo from './logo.svg';
 import './App.css';
 //import LoginForm from './components/Login';
-import SignForm from './components/SignForm';
-
+// import SignForm from './components/SignForm';
+import CharityBeneficiariesManagement from './components/CharityBeneficiariesManagement';
 function App() {
   return (
     <div>
       {/* <CardComponent /> */}
       {/* <CharityPage /> */}
-      {/* <CharityBeneficiariesManagement /> */}
+      <CharityBeneficiariesManagement />
       {/* <LoginForm /> */}
-      <SignForm />
+       {/* <SignForm /> */}
 
     </div>
   );
-}
+} 
+
 
 export default App;
