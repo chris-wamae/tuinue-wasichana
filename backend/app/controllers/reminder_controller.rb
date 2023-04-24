2e2830338c148564a7b202ac31d5b645a5277f52
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-04-24 12:00:43 +0300

    deleted some files

--- backend/app/controllers/reminder_controller.rb
+++ backend/app/controllers/reminder_controller.rb
@@ -1,31 +1,31 @@
 class ReminderController < ApplicationController
-    def create
-        @reminder = Reminder.new(reminder_params)
-        if @reminder.save
-          render json: @reminder, status: :created
-        else
-          render json: { errors: @reminder.errors.full_messages }, status: :unprocessable_entity
-        end
+  def create
+      @reminder = Reminder.new(reminder_params)
+    if @reminder.save
+      render json: @reminder, status: :created
+    else
+      render json: { errors: @reminder.errors.full_messages }, status: :unprocessable_entity
     end
+  end
     
-    def update
-        @reminder = Reminder.find(params[:id])
-        if @reminder.update(reminder_params)
-          render json: @reminder
-        else
-          render json: { errors: @reminder.errors.full_messages }, status: :unprocessable_entity
-        end
+  def update
+      @reminder = Reminder.find(params[:id])
+    if @reminder.update(reminder_params)
+      render json: @reminder
+    else
+      render json: { errors: @reminder.errors.full_messages }, status: :unprocessable_entity
     end
+  end
     
-    def destroy
-        @reminder = Reminder.find(params[:id])
-        @reminder.destroy
-        head :no_content
-    end
+  def destroy
+    @reminder = Reminder.find(params[:id])
+    @reminder.destroy
+    head :no_content
+  end
     
-      private
+    private
     
-    def reminder_params
-        params.require(:reminder).permit(:title, :description, :date_time, :user_id)
-    end
+  def reminder_params
+    params.require(:reminder).permit(:title, :description, :date_time, :user_id)
+  end
 end
