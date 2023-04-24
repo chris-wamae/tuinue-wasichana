506456793d94b92880604ae879a4731620cb4154
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-04-24 15:58:36 +0300

    added routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "tests#index"
-  resources :beneficiaries
-  resources :donations
+  # resources :beneficiaries
+  # resources :donations
   resources :inventory, only: [:create, :update]
   resources :reminders, only: [:create, :update, :destroy]
+  get '/inventory', to: 'inventory#view_inventories'
   get '/charities/:charity_id/anonymous_donations', to: 'donations#anonymous_donations', as: 'anonymous_donations'
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,5 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-
+# Seed data for inventories table
+Inventory.create(item_name: "Blankets", quantity: 10, beneficiary_id: 1, charity_id: 1)
+Inventory.create(item_name: "Food Packs", quantity: 20, beneficiary_id: 2, charity_id: 2)
+Inventory.create(item_name: "Medicine", quantity: 5, beneficiary_id: 3, charity_id: 3)
 
