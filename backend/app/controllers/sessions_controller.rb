9accd09262f5fbb9f1925891e2a08c81ad0043f9
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-04-25 13:32:34 +0300

    added json return for users

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < Devise::SessionsController
+  respond_to :json
+  skip_before_action :require_no_authentication, only: [:create]
+
+
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    sign_in(resource_name, resource)
+
+    render json: {
+      message: 'Signed in successfully',
+      user: resource.as_json(except: [:encrypted_password, :reset_password_token, :confirmation_token, :unlock_token]),
+      status: 200
+    }
+  end
     protected
   
     def respond_to_on_destroy
