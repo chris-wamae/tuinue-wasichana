5133b2c7d79bd392418a96fb472e3d350e84ba5b
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-04-25 09:23:47 +0300

    resolved conflict

--- backend/db/migrate/20230418101055_create_reminders.rb
+++ backend/db/migrate/20230418101055_create_reminders.rb
@@ -1,11 +1,11 @@
 class CreateReminders < ActiveRecord::Migration[7.0]
-  def change
-    create_table :reminders do |t|
-      t.integer :donor_id
-      t.integer :charity_id
-      t.date :reminder_date
-
-      t.timestamps
+    def change
+      create_table :reminders do |t|
+        t.integer :donor_id
+        t.integer :charity_id
+        t.date :reminder_date
+  
+        t.timestamps
+      end
     end
   end
-end
