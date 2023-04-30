7310c54421be647827c9773336c8b98757d5f78c
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-04-30 17:47:12 +0300

    created #index:display beneficiaries for a specific charity

--- backend/app/controllers/beneficiaries_controller.rb
+++ backend/app/controllers/beneficiaries_controller.rb
@@ -1,2 +1,20 @@
 class BeneficiariesController < ApplicationController
+    before_action :find_charity, only: [:index]
+    # before_action :find_beneficiary, only: [:update,]
+    #displays all inventories
+    def all_beneficiaries
+        render json: Beneficiary.all, status: :ok
+    end
+
+    #displays all beneficiaries for a specific charity 
+    def index 
+        beneficiaries = @charity.beneficiaries
+        render json: beneficiaries, status: :ok
+    end
+
+    private
+    def find_charity
+        user = User.charity.find(params[:charity_id])
+        @charity = user.becomes(Charity)
+    end
 end
